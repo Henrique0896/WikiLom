61115d3fc9d9b55a2df45b37d3d5cf74528f07bd
henrique <barrosohenriquelima@gmail.com>
2021-03-22 20:16:42 -0300

    43

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -7,14 +7,25 @@ import json
 from ..models.tables import LearningObject
 
 pages_found = None
+instance_list = None
+
 
 
 # Lista objetos do Banco
 @app.route("/index")
 @app.route("/")
 def index():
+    global instance_list
     instance_list = db.list("learning_object")
-    return render_template('index.html', materias=instance_list)
+    if len(list(instance_list)) != 0:
+        i = 0
+        pages = []
+        for page in instance_list:
+            pages.append([ page, str(i) ])
+            i=i+1
+    else:
+        pages = None
+    return render_template('index.html', materias=pages)
 
 
 # Pesquisa qual materia será adicionada
@@ -22,6 +33,7 @@ def index():
 def adicionar():
     form = addMateria()
     global pages_found
+    pages_found = None
     pages = None
     if form.validate_on_submit():
         pages_found = wikipedia.search(form.pesquisa.data)
@@ -35,7 +47,7 @@ def adicionar():
             pass
     else:
         print(form.errors)
-   
+
     return render_template('adicionar.html', form=form, pages=pages)
 
 
@@ -51,3 +63,14 @@ def adicionarPage(pageNumber):
     db.create("learning_object", learning_object)
 
     return render_template('adicionado.html', page_title=page_title)
+
+
+
+#Deletar materia ao banco
+@app.route("/excluir/<pageNumber>", methods=['DELETE', 'GET' ])
+def excluirPage(pageNumber):
+    global instance_list
+    page_title = instance_list[int(pageNumber)]['geral']['titulo']
+    db.delete("learning_object", instance_list[int(pageNumber)])
+
+    return render_template('removido.html', page_title=page_title)
